f71ba2dec845c04cb0c5143c47ab7e39d780ebbb
victorious-messaging-4309 <wildayk@gmail.com>
2019-03-17 18:48:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -17,7 +17,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
        :memorial_day => ["BBQ"]
      }
    }
-   
+
    holiday_hash[:summer][:fourth_of_july][1]
   # return the second element in the 4th of July array
 end
@@ -64,10 +64,3 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 
 end
-
-
-
-
-
-
-
