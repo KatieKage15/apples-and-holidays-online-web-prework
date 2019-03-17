13670902af2f100f71a748a6e9fa17f20620e032
victorious-messaging-4309 <wildayk@gmail.com>
2019-03-17 19:01:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -31,7 +31,6 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   end
 end
 
-
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
@@ -41,7 +40,8 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  holiday_hash[season][holiday_name] = supply_array
+  holiday_hash 
 end
 
 def all_winter_holiday_supplies(holiday_hash)
